ec34657f2531bc1ebfc074d5320485419b8eede8
wanjikuKim-collab <wanjikukimani24@gmail.com>
2024-03-19 21:38:39 +0300

    Create a function that adds two numbers

--- activities/src/bin/a2.rs
+++ activities/src/bin/a2.rs
@@ -8,4 +8,11 @@
 // * Use a function to display the result
 // * Use the "{:?}" token in the println macro to display the result
 
+
+// * Use a function to add two numbers together
+fn add(a: i32, b:i32)->i32 {
+    a + b
+}
+
+// * Use a function to display the result
 fn main() {}
